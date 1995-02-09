d21336d699898344c0af816edf1064474af32b44
Sebastian Hammer <quinn@indexdata.com>
1995-02-09 15:52:34 +0000

    Initial revision

---
+++ Makefile
@@ -0,0 +1,51 @@
+# Copyright (C) 1994, Index Data I/S 
+# All rights reserved.
+# Sebastian Hammer, Adam Dickmeiss
+# $Id: Makefile,v 1.1 1995-02-09 15:57:16 quinn Exp $
+
+#CC=
+SHELL=/bin/sh
+MAKE=make
+SUBDIR=odr asn yazlib
+
+all:
+	for i in $(SUBDIR); do cd $$i; if $(MAKE) CFLAGS="$(CFLAGS)"; then cd ..; else exit 1; fi; done
+
+dep depend:
+	for i in $(SUBDIR); do cd $$i; if $(MAKE) depend; then cd ..; else exit 1; fi; done
+
+clean:
+	for i in $(SUBDIR); do (cd $$i; $(MAKE) clean); done
+
+cleanup:
+	rm -f `find $(SUBDIR) -name "*.[oa]" -print`
+	rm -f `find $(SUBDIR) -name "core" -print`
+	rm -f `find $(SUBDIR) -name "errlist" -print`
+	rm -f `find $(SUBDIR) -name "a.out" -print`
+
+distclean: cleanup clean
+	for i in $(SUBDIR); do (cd $$i; \
+		mv Makefile Makefile.old; \
+		sed '/^#Depend/q' <Makefile.old >Makefile; \
+		rm Makefile.old); done
+
+usedepend1:
+	for i in $(SUBDIR); do (cd $$i; \
+		mv Makefile Makefile.tmp; \
+		sed 's/^if/#if/' <Makefile.tmp|sed 's/^include/#include/'| \
+		sed 's/^endif/#endif/' | \
+		sed 's/^depend: depend2/depend: depend1/g' >Makefile; \
+		rm Makefile.tmp); done
+
+usedepend2:
+	for i in $(SUBDIR); do (cd $$i; \
+		mv Makefile Makefile.tmp; \
+		sed '/^#Depend/q' <Makefile.tmp| \
+		sed 's/^#if/if/' |sed 's/^#include/include/'| \
+		sed 's/^#endif/endif/' | \
+		sed 's/^depend: depend1/depend: depend2/g' >Makefile; \
+		rm Makefile.tmp); done
+
+wc:
+	wc `find . -name '*.[ch]'`
+	
---
+++ asn/test.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <odr.h>
+#include <odr_use.h>
+#include <proto.h>
+
+int main()
+{
+    int i;
+    unsigned char buf[10000];
+    struct odr o;
+    Z_APDU apdu, *papdu, *papdu2;
+    Z_SearchRequest sreq;
+    int a1, a2, a3, a4, a5, a6, a7, a8, a9, a10;
+    bool_t rep;
+    static char *names[] = {"Erik", "William", "George", "Bob"};
+    Z_Query query;
+    Z_RPNQuery rpnquery;
+    Odr_oid att[] = { 1, 2, 3, 4, 5, 6, -1};
+    Z_RPNStructure rpnstructure;
+    Z_Operand operand;
+    Z_AttributesPlusTerm apt;
+    Odr_oct term;
+
+    papdu = &apdu;
+    apdu.which = Z_APDU_searchRequest;
+    apdu.u.searchRequest = &sreq;
+    a1 = 1000; sreq.smallSetUpperBound = &a1;
+    a2 = 2000; sreq.largeSetLowerBound = &a2;
+    a3 = 100; sreq.mediumSetPresentNumber = &a3;
+    rep = 1; sreq.replaceIndicator = &rep;
+    sreq.resultSetName = "FOOBAR";
+    sreq.num_databaseNames = 4;
+    sreq.databaseNames = names;
+    sreq.smallSetElementSetNames = 0;
+    sreq.mediumSetElementSetNames = 0;
+    sreq.preferredRecordSyntax = 0;
+    query.which = Z_Query_type_1;
+    query.u.type_1 = &rpnquery;
+    sreq.query = &query;
+    rpnquery.attributeSetId = att;
+    rpnquery.RPNStructure = &rpnstructure;
+    rpnstructure.which = Z_RPNStructure_simple;
+    rpnstructure.u.simple = &operand;
+    operand.which = Z_Operand_APT;
+    operand.u.attributesPlusTerm = &apt;
+    apt.num_attributes=0;
+    apt.attributeList = 0;
+    apt.term = &term;
+    term.buf = "BARFOO";
+    term.len = term.size = strlen(term.buf);
+
+    o.buf = buf;
+    o.bp=o.buf;
+    o.left = o.buflen = 10000;
+    o.direction = ODR_PRINT;
+    o.print = stdout;
+    o.indent = 0;
+    o.t_class = -1;
+
+    printf("status=%d\n", z_APDU(&o, &papdu, 0));
+
+    return 0;
+
+    o.direction = ODR_DECODE;
+    o.bp = o.buf;
+
+    z_APDU(&o, &papdu2, 0);
+}    
