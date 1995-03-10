e699f6c0dc97950dce23a228eeabc80f17edd6d3
Sebastian Hammer <quinn@indexdata.com>
1995-03-10 18:22:18 +0000

    Just the usual copyright-header.

---
+++ header.c
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 1994, Index Data I/S 
+ * All rights reserved.
+ * Sebastian Hammer, Adam Dickmeiss
+ *
+ * $Log: header.c,v $
+ * Revision 1.1  1995-03-10 18:22:18  quinn
+ * Just the usual copyright-header.
+ *
+ */
+
