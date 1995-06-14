9edede4a23ba78a2c4a463eabbe7623e7f8d8fbd
Sebastian Hammer <quinn@indexdata.com>
1995-06-14 12:34:55 +0000

    Moved version.h to include/

---
+++ include/yaz-version.h
@@ -0,0 +1,27 @@
+/*
+ * Current software version.
+ *
+ * $Log: yaz-version.h,v $
+ * Revision 1.1  1995-06-14 12:34:55  quinn
+ * Moved version.h to include/
+ *
+ * Revision 1.4  1995/06/07  14:43:57  quinn
+ * Various work towards first public release. Specifically, the CLOSE
+ * service has been added.
+ *
+ * Revision 1.3  1995/05/29  09:52:38  quinn
+ * Second pre-release.
+ *
+ * Revision 1.2  1995/05/16  10:22:47  quinn
+ * 0.1 beta
+ * Fairly stable version with structure in place for asynchronous server
+ * activity (the backend doesn't have complete support for this yet).
+ *
+ * Revision 1.1  1995/05/16  08:49:38  quinn
+ * Introduced version control
+ *
+ */
+
+#ifndef YAZ_VERSION
+#define YAZ_VERSION "0.21 beta"
+#endif
