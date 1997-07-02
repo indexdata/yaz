e7a11f55c79f2ddf7a2cdaa898cfe57ce2fe404a
Adam Dickmeiss <adam@indexdata.dk>
1997-07-02 14:12:52 +0000

    Changed header to 1997.

--- header.c
+++ header.c
@@ -1,10 +1,13 @@
 /*
- * Copyright (c) 1995-1996, Index Data.
+ * Copyright (c) 1995-1997, Index Data.
  * See the file LICENSE for details.
  * Sebastian Hammer, Adam Dickmeiss
  *
  * $Log: header.c,v $
- * Revision 1.4  1996-10-29 13:33:52  adam
+ * Revision 1.5  1997-07-02 14:12:52  adam
+ * Changed header to 1997.
+ *
+ * Revision 1.4  1996/10/29 13:33:52  adam
  * Updated.
  *
  * Revision 1.3  1995/05/16 08:50:16  quinn
