d953d065a4ed1abd68cec29f93e061a2b8934cd8
Adam Dickmeiss <adam@indexdata.dk>
1999-12-08 13:21:02 +0000

    Minor fix.

--- z39.50/comp.sh
+++ z39.50/comp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# $Id: comp.sh,v 1.1 1999-12-08 13:14:12 adam Exp $
+# $Id: comp.sh,v 1.2 1999-12-08 13:21:02 adam Exp $
 # simple script to make all compiled code.
-../util/yc.tcl -d z.tcl -i yaz -I ../include $(YCFLAGS) z3950v3.asn
-../util/yc.tcl -d z.tcl -i yaz -I ../include $(YCFLAGS) datetime.asn
-../util/yc.tcl -d z.tcl -i yaz -I ../include $(YCFLAGS) univres.asn
-../util/yc.tcl -d z.tcl -i yaz -I ../include $(YCFLAGS) esupdate.asn
+../util/yc.tcl -d z.tcl -i yaz -I ../include z3950v3.asn
+../util/yc.tcl -d z.tcl -i yaz -I ../include datetime.asn
+../util/yc.tcl -d z.tcl -i yaz -I ../include univres.asn
+../util/yc.tcl -d z.tcl -i yaz -I ../include esupdate.asn
