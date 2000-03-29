670bcd00bdc2b4559fde47a9b38cbb855b89aa48
Adam Dickmeiss <adam@indexdata.dk>
2000-03-29 15:07:16 +0000

    Updated for PHP.

--- doc/indexdata.sh
+++ doc/indexdata.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# $Id: indexdata.sh,v 1.1 2000-03-03 10:18:50 adam Exp $
+# $Id: indexdata.sh,v 1.2 2000-03-29 15:07:16 adam Exp $
 # Script to convert documentation to indexdata.dk style...
 for i in ${package}*.html; do
-	j=`basename $i html`shtml
-	sed '-es/<BODY>/<BODY BGCOLOR="#FFFFFF" TEXT="#000000"><TABLE BORDER="0" CELLSPACING="10" CELLPADDING="0"><TR><TD ALIGN="LEFT" VALIGN="TOP" ROWSPAN="2" WIDTH="100" BGCOLOR="#FFFFFF"><!--#config errmsg="No table, contact info@indexdata.dk"--><!--#include virtual="..\/buttonbar.shtml"--><\/TD><TD VALIGN="TOP">/g' '-es/<\/BODY>/<\/TD><\/TR><\/TABLE><\/BODY>/g' '-es/\(HREF=\"yaz.*\)\.html/\1.shtml/g'  < $i >$j
+	j=`basename $i html`php
+	awk 'BEGIN { body = 0 } /<\/BODY/ { body = 0; print "<?php id_footer(); ?>" } { if (body == 1) print $0} /<BODY>/ { body = 1; print "<?php require(\"../id_common.inc\"); id_header(\"YAZ Documentation\"); ?>"; } ' < $i | sed '-es/\(HREF=\"yaz.*\)\.html/\1.php/g' >$j
 done
