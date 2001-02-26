69392bd6f5e182f413fd406c598d4ae0108f166c
Adam Dickmeiss <adam@indexdata.dk>
2001-02-26 22:52:54 +0000

    Script buildconf script is quiet.

--- buildconf.sh
+++ buildconf.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.1 2001-02-21 09:03:34 adam Exp $
-aclocal
-libtoolize --force
-automake -a
-autoconf
+# $Id: buildconf.sh,v 1.2 2001-02-26 22:52:54 adam Exp $
+aclocal || exit 1
+libtoolize --force >/dev/null 2>&1 || exit 2
+automake -a >/dev/null 2>&1 || exit 3
+autoconf || exit 4
+if [ -f config.cache ]; then
+	rm config.cache
+fi
