49bb5732e64ef1bd772d5393450815684eac39b6
Adam Dickmeiss <adam@indexdata.dk>
2001-03-13 18:10:39 +0000

    Updated.

--- header.c
+++ header.c
@@ -1,29 +1,8 @@
 /*
- * Copyright (c) 1995-1999, Index Data.
+ * Copyright (c) 1995-2001, Index Data.
  * See the file LICENSE for details.
- * Sebastian Hammer, Adam Dickmeiss
- 9
- * $Log: header.c,v $
- * Revision 1.7  1999-01-08 11:23:13  adam
- * Added const modifier to some of the BER/ODR encoding routines.
  *
- * Revision 1.6  1998/01/05 09:03:37  adam
- * 1998.
- *
- * Revision 1.5  1997/07/02 14:12:52  adam
- * Changed header to 1997.
- *
- * Revision 1.4  1996/10/29 13:33:52  adam
- * Updated.
- *
- * Revision 1.3  1995/05/16 08:50:16  quinn
- * License, documentation, and memory fixes
- *
- * Revision 1.2  1995/05/15  11:55:15  quinn
- * Documentation & other details.
- *
- * Revision 1.1  1995/03/10  18:22:18  quinn
- * Just the usual copyright-header.
+ * $Id: header.c,v 1.8 2001-03-13 18:10:39 adam Exp $
  *
  */
 
