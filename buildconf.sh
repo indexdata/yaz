db9768c3988f9536c898250880c13923a2dde32d
Adam Dickmeiss <adam@indexdata.dk>
2001-11-06 17:03:50 +0000

    More verbose.

--- buildconf.sh
+++ buildconf.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.6 2001-10-23 22:44:32 adam Exp $
-aclocal || exit 1
+# $Id: buildconf.sh,v 1.7 2001-11-06 17:03:50 adam Exp $
+aclocal 
 libtoolize --automake --force || exit 2
-automake -a >/dev/null 2>&1 || exit 3
+automake -a  || exit 3
 autoconf || exit 4
 if [ -f config.cache ]; then
 	rm config.cache
