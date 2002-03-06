09abca5ee8cb11137decdfa7d10b822173ad5fe1
Adam Dickmeiss <adam@indexdata.dk>
2002-03-06 01:38:51 +0000

    Add yaz-date.h again

--- buildconf.sh
+++ buildconf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.8 2002-02-28 13:21:16 adam Exp $
+# $Id: buildconf.sh,v 1.9 2002-03-06 01:38:51 adam Exp $
 aclocal 
 libtoolize --automake --force || exit 2
 automake -a  || exit 3
@@ -7,4 +7,4 @@ autoconf || exit 4
 if [ -f config.cache ]; then
 	rm config.cache
 fi
-#util/cvs-date.tcl include/yaz/yaz-date.h
+util/cvs-date.tcl include/yaz/yaz-date.h
