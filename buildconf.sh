f722c8d9517ec491e2469cdc91a3751dd5e7a6df
Adam Dickmeiss <adam@indexdata.dk>
2002-03-16 11:46:18 +0000

    buildconf.sh more verbose

--- buildconf.sh
+++ buildconf.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.9 2002-03-06 01:38:51 adam Exp $
+# $Id: buildconf.sh,v 1.10 2002-03-16 11:46:18 adam Exp $
 aclocal 
-libtoolize --automake --force || exit 2
-automake -a  || exit 3
-autoconf || exit 4
+libtoolize --automake --force 
+automake -a 
+autoconf
 if [ -f config.cache ]; then
 	rm config.cache
 fi
