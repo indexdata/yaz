c6e53a084e3819438c2a1e7de947a655f708f15b
Adam Dickmeiss <adam@indexdata.dk>
2002-05-22 14:27:17 +0000

    Bump version

--- include/yaz/yaz-version.h
+++ include/yaz/yaz-version.h
@@ -1,12 +1,12 @@
 /*
  * Current software version.
  *
- * $Id: yaz-version.h,v 1.15 2002-05-13 18:34:53 adam Exp $
+ * $Id: yaz-version.h,v 1.16 2002-05-22 14:27:17 adam Exp $
  */
 #ifndef YAZ_VERSION
 
-#define YAZ_VERSION "1.8.7"
-#define YAZ_VERSIONL 0x010807
+#define YAZ_VERSION "1.8.8dev"
+#define YAZ_VERSIONL 0x010808
 
 #define YAZ_DATE 1
 
