d30f275ba6239b9e644ea60cd123487173704824
Adam Dickmeiss <adam@indexdata.dk>
2002-08-27 13:18:26 +0000

    Remove dead comment

--- util/atoin.c
+++ util/atoin.c
@@ -1,21 +1,8 @@
 /*
- * Copyright (c) 1997, Index Data
+ * Copyright (c) 1997-2002, Index Data
  * See the file LICENSE for details.
- * Sebastian Hammer, Adam Dickmeiss
  *
- * $Log: atoin.c,v $
- * Revision 1.4  2002-08-26 09:25:56  adam
- * Buffer overflow fix
- *
- * Revision 1.3  2000/02/29 13:44:55  adam
- * Check for config.h (currently not generated).
- *
- * Revision 1.2  1999/11/30 13:47:12  adam
- * Improved installation. Moved header files to include/yaz.
- *
- * Revision 1.1  1997/09/04 07:52:27  adam
- * Moved atoi_n function to separate source file.
- *
+ * $Id: atoin.c,v 1.5 2002-08-27 13:18:26 adam Exp $
  */
 
 #if HAVE_CONFIG_H
@@ -39,33 +26,3 @@ int atoi_n (const char *buf, int len)
     return val;
 }
 
-/*
-  UCS-4 <- ISO-8859-1
-  (unsigned long) ch = ch & 255;
-
-  ISO-8859-1 -> UCS-4
-  if (ch > 255)
-      invalid sequence
-  else
-      ch = ch;
-
-  UCS-4 -> UTF-8
-  if (ch <= 0x7f)
-      ch = ch;
-  else if c(h <= 0x7ff)
-  {
-      str[0] = 0xc0 + (ch & (0xff < 6));
-      str[1] = 0x80 + ch & 0x7ff;
-  }
-  else if (ch <= 0xffff)
-  {
-
-
-
-  }
-
-
-
-*/ 
-  
-    
