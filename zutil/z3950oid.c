4f0e281e16293a8c6403d7ad1265a13eae911006
Adam Dickmeiss <adam@indexdata.dk>
2002-09-20 22:30:01 +0000

    Indentation

--- zutil/z3950oid.c
+++ zutil/z3950oid.c
@@ -2,7 +2,7 @@
  * Copyright (c) 1995-2002, Index Data
  * See the file LICENSE for details.
  *
- * $Id: z3950oid.c,v 1.3 2002-08-29 19:36:09 ja7 Exp $
+ * $Id: z3950oid.c,v 1.4 2002-09-20 22:30:01 adam Exp $
  */
 
 #if HAVE_CONFIG_H
@@ -52,29 +52,31 @@ const char *yaz_z3950oid_to_str (Odr_oid *oid, int *oid_class)
 }
 
 
-const char* yaz_z3950_oid_value_to_str(oid_value ov, oid_class oc) {
-	struct oident tmpentry;
-	int tmp_oid[OID_SIZE];
-	
-
-	tmpentry.proto = PROTO_Z3950;
-	tmpentry.oclass = oc;
-	tmpentry.value = ov; 
-	
-	if( oid_ent_to_oid(&tmpentry,tmp_oid) ) 
-	{
-		return tmpentry.desc;
-	} 
-	else 
-	{
-		return "";
-	};		  
-};
+const char* yaz_z3950_oid_value_to_str(oid_value ov, oid_class oc)
+{
+    struct oident tmpentry;
+    int tmp_oid[OID_SIZE];
+     
+    tmpentry.proto = PROTO_Z3950;
+    tmpentry.oclass = oc;
+    tmpentry.value = ov; 
+    
+    if( oid_ent_to_oid(&tmpentry,tmp_oid) ) 
+    {
+        return tmpentry.desc;
+    } 
+    else 
+    {
+        return "";
+    }
+}
 
 
 /*
  * Local variables:
- * tab-width: 4
+ * tab-width: 8
  * c-basic-offset: 4
  * End:
+ * vim600: sw=4 ts=8 fdm=marker
+ * vim<600: sw=4 ts=8
  */
