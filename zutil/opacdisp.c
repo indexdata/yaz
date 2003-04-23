d7e3f387cddf4ac8dee3ca0d90992d052834da95
Adam Dickmeiss <adam@indexdata.dk>
2003-04-23 12:31:07 +0000

    Prepare for OPAC display

---
+++ zutil/opacdisp.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2003, Index Data.
+ * See the file LICENSE for details.
+ *
+ * $Id: opacdisp.c,v 1.1 2003-04-23 12:31:07 adam Exp $
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include <yaz/proto.h>
+
+void yaz_display_OPAC(WRBUF wrbuf, Z_OPACRecord *r, int flags)
+{
+
+
+}
