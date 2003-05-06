0d224465e46c5886cfdd5002b34cd3b239043fb5
Adam Dickmeiss <adam@indexdata.dk>
2003-05-06 12:09:10 +0000

    Call automake twice to avoid missing depcomp

--- buildconf.sh
+++ buildconf.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.15 2003-05-06 11:37:18 adam Exp $
+# $Id: buildconf.sh,v 1.16 2003-05-06 12:09:10 adam Exp $
 set -x
 aclocal
 libtoolize --automake --force 
+automake --add-missing 
 automake --add-missing 
 autoconf
 if [ -f config.cache ]; then
