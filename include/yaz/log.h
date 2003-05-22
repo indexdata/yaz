6dc4539f74395fa8f95596263d86edc1281bc399
Heikki Levanto <heikki@indexdata.dk>
2003-05-22 15:41:58 +0000

    Fixed a silly typo

--- include/yaz/log.h
+++ include/yaz/log.h
@@ -23,7 +23,7 @@
  * LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
  * OF THIS SOFTWARE.
  *
- * $Id: log.h,v 1.10 2003-05-22 13:15:07 heikki Exp $
+ * $Id: log.h,v 1.11 2003-05-22 15:41:58 heikki Exp $
  */
 
 #ifndef LOG_H
@@ -45,7 +45,7 @@ YAZ_BEGIN_CDECL
 #define LOG_MALLOC 0x0080     /* debugging mallocs */
 #define LOG_NOTIME 0x0100     /* do not output date and time */
 #define LOG_APP2   0x0200     /* For application-level events, such as api calls */
-#define LOG_APP3   0x0200     /* For more application-level events */
+#define LOG_APP3   0x0400     /* For more application-level events */
 
 #define LOG_ALL   (0xffff&~LOG_MALLOC&~LOG_NOTIME)
 
