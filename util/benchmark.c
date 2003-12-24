9e678d07eb1deabb1cd342c6510512e9c82685cf
Mike Taylor <mike@indexdata.com>
2003-12-24 16:23:43 +0000

    Empty benchmarking application

---
+++ util/benchmark.c
@@ -0,0 +1,18 @@
+/* $Id: benchmark.c,v 1.1 2003-12-24 16:23:43 mike Exp $
+   Copyright (C) 2003
+   Index Data Aps
+
+   This file is part of the YAZ toolkit.
+
+   See the file LICENSE.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <yaz/zoom.h>
+
+
+int main(int argc, char **argv)
+{
+}
