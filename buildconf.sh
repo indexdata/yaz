afabe5eef1993c5b71602942f2e50a0d27ecbf33
Marc Cromme <marc@indexdata.dk>
2004-02-17 09:23:16 +0000

    build-dependencies added

--- buildconf.sh
+++ buildconf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.16 2003-05-06 12:09:10 adam Exp $
+# $Id: buildconf.sh,v 1.17 2004-02-17 09:23:16 marc Exp $
 set -x
 aclocal
 libtoolize --automake --force 
@@ -9,3 +9,30 @@ autoconf
 if [ -f config.cache ]; then
 	rm config.cache
 fi
+
+MESSAGE=" 
+
+Build the Makefiles with the configure command.
+  ./configure [--someoption=somevalue ...]
+
+For help on options or configuring run
+  ./configure --help
+
+Build and install binaries with the usual
+  make
+  make check
+  make install
+
+Build distribution tarball with
+  make dist
+
+Verify distribution tarball with
+  make distcheck
+
+Or just build the Debian packages without configuring
+  dpkg-buildpackage -rfakeroot
+
+When building from a CVS checkout, you need these Debian tools:
+  bison, docbook-utils, docbook, docbook-xml, docbook-dsssl, jade, jadetex,
+  libxml2-dev, libssl-dev, libreadline4-dev, libwrap0-dev
+"
