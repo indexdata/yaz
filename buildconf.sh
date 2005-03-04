c9d151d16586a20585bfb92607a6d578ceb07770
Adam Dickmeiss <adam@indexdata.dk>
2005-03-04 12:00:00 +0000

    buildconf script configures directlry when -d / -c is given

--- buildconf.sh
+++ buildconf.sh
@@ -1,16 +1,44 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.19 2004-04-30 19:34:05 adam Exp $
+# $Id: buildconf.sh,v 1.20 2005-03-04 12:00:00 adam Exp $
 set -x
 aclocal -I .
 libtoolize --automake --force 
 automake --add-missing 
 automake --add-missing 
 autoconf
+set -
 if [ -f config.cache ]; then
 	rm config.cache
 fi
 
-MESSAGE=" 
+enable_configure=false
+enable_help=true
+sh_flags=""
+conf_flags=""
+case $1 in
+    -d)
+	sh_flags="-g -Wall"
+	enable_configure=true
+	enable_help=false
+	shift
+	;;
+    -c)
+	sh_flags=""
+	enable_configure=true
+	enable_help=false
+	shift
+	;;
+esac
+
+if $enable_configure; then
+    if test -n "$sh_flags"; then
+	CFLAGS="$sh_flags" ./configure $*
+    else
+	./configure $*
+    fi
+fi
+if $enable_help; then
+    cat <<EOF
 
 Build the Makefiles with the configure command.
   ./configure [--someoption=somevalue ...]
@@ -35,4 +63,5 @@ Or just build the Debian packages without configuring
 When building from a CVS checkout, you need these Debian tools:
   bison, docbook-utils, docbook, docbook-xml, docbook-dsssl, jade, jadetex,
   libxml2-dev, libssl-dev, libreadline4-dev, libwrap0-dev, any tcl
-"
+EOF
+fi
