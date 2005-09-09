fd446dea5b8ff2ac645272686f2be5308fd71f43
Adam Dickmeiss <adam@indexdata.dk>
2005-09-09 10:33:45 +0000

    Major log system cleanup. Added various locks for the shared mask_names global structure. Also added mutex around yaz_log core code. It seems that vsnprintf or similar is not thread-safe after all (at least on Solaris 9).

--- src/log.c
+++ src/log.c
@@ -2,7 +2,7 @@
  * Copyright (C) 1995-2005, Index Data ApS
  * See the file LICENSE for details.
  *
- * $Id: log.c,v 1.25 2005-06-25 15:46:04 adam Exp $
+ * $Id: log.c,v 1.26 2005-09-09 10:33:45 adam Exp $
  */
 
 /**
@@ -75,7 +75,7 @@ static int l_max_size = 1024*1024*1024;
 static struct {
     int mask;
     char *name;
-}  mask_names[MAX_MASK_NAMES] =
+} mask_names[MAX_MASK_NAMES] =
 {
     { YLOG_FATAL,  "fatal"},
     { YLOG_DEBUG,  "debug"},
@@ -83,14 +83,14 @@ static struct {
     { YLOG_LOG,    "log"  },
     { YLOG_ERRNO,  ""},
     { YLOG_MALLOC, "malloc"},
-    { YLOG_APP,     "app"  },
+    { YLOG_APP,    "app"  },
     { YLOG_NOTIME, "notime" },
     { YLOG_APP2,   "app2" }, 
     { YLOG_APP3,   "app3" },
     { YLOG_ALL,    "all"  },
     { YLOG_FLUSH,  "flush" },
     { YLOG_LOGLVL, "loglevel" }, 
-    { 0,          "none" },
+    { 0,           "none" },
     { 0, NULL }
     /* the rest will be filled in if the user defines dynamic modules*/
 };  
@@ -114,8 +114,7 @@ FILE *yaz_log_file(void)
 
 void yaz_log_init_file (const char *fname)
 {
-    if (!mutex_init_flag)
-        init_mutex();
+    init_mutex();
     if (fname)
     {
         strncpy(l_fname, fname, sizeof(l_fname)-1);
@@ -129,8 +128,7 @@ void yaz_log_init_file (const char *fname)
 void yaz_log_reopen(void)
 {
     FILE *new_file;
-    if (!mutex_init_flag)
-        init_mutex();
+    init_mutex();
     if (!l_file)
         l_file = stderr;
     if (!*l_fname)
@@ -174,8 +172,7 @@ static void rotate_log()
 
 void yaz_log_init_level (int level)
 {
-    if (!mutex_init_flag)
-        init_mutex();
+    init_mutex();
     if ( (l_level & YLOG_FLUSH) != (level & YLOG_FLUSH) )
     {
         l_level = level;
@@ -184,18 +181,26 @@ void yaz_log_init_level (int level)
         l_level = level;
     if (l_level  & YLOG_LOGLVL)
     {  /* dump the log level bits */
-        char *bittype="Static ";
-        int i;
-        yaz_log(YLOG_LOGLVL,"Setting log level to %d = 0x%08x",l_level,l_level);
-        for (i = 0; mask_names[i].name; i++)
+        const char *bittype = "Static ";
+        int i, sz;
+
+        yaz_log(YLOG_LOGLVL, "Setting log level to %d = 0x%08x",
+                l_level, l_level);
+        /* determine size of mask_names (locked) */
+        nmem_mutex_enter(log_mutex);
+        for (sz = 0; mask_names[sz].name; sz++)
+            ;
+        nmem_mutex_leave(log_mutex);
+        /* second pass without lock */
+        for (i = 0; i < sz; i++)
             if (mask_names[i].mask && *mask_names[i].name)
-                if (strcmp(mask_names[i].name,"all")!=0)
+                if (strcmp(mask_names[i].name, "all") != 0)
                 {
-                    yaz_log(YLOG_LOGLVL,"%s log bit %08x '%s' is %s",
-                        bittype, mask_names[i].mask, mask_names[i].name,
-                        (level & mask_names[i].mask)?  "ON": "off");
-                if (mask_names[i].mask>YLOG_LAST_BIT)
-                    bittype="Dynamic";
+                    yaz_log(YLOG_LOGLVL, "%s log bit %08x '%s' is %s",
+                            bittype, mask_names[i].mask, mask_names[i].name,
+                            (level & mask_names[i].mask)?  "ON": "off");
+                    if (mask_names[i].mask > YLOG_LAST_BIT)
+                        bittype = "Dynamic";
                 }
     }
 }
@@ -218,8 +223,7 @@ void yaz_log_init_prefix2 (const char *prefix)
 
 void yaz_log_init(int level, const char *prefix, const char *fname)
 {
-    if (!mutex_init_flag)
-        init_mutex();
+    init_mutex();
     yaz_log_init_level (level);
     yaz_log_init_prefix (prefix);
     if (fname && *fname)
@@ -264,18 +268,16 @@ void yaz_log(int level, const char *fmt, ...)
 
     if (!(level & l_level))
         return;
-    if (!mutex_init_flag)
-        init_mutex();
+    init_mutex();
+    nmem_mutex_enter(log_mutex);
     if (!l_file)
         l_file = stderr;
     
     if ((l_file != stderr) && (l_max_size>0))
     {
-        nmem_mutex_enter (log_mutex);
         flen = ftell(l_file);
         if (flen>l_max_size) 
             rotate_log();
-        nmem_mutex_leave (log_mutex);
     }
 
     *flags = '\0';
@@ -289,6 +291,7 @@ void yaz_log(int level, const char *fmt, ...)
                 level &= ~mask_names[i].mask;
             }
         }
+
     va_start(ap, fmt);
 #ifdef WIN32
     _vsnprintf(buf, sizeof(buf)-1, fmt, ap);
@@ -323,6 +326,7 @@ void yaz_log(int level, const char *fmt, ...)
         fflush(l_file);
     if (end_hook_func)
         (*end_hook_func)(o_level, buf, end_hook_info);
+    nmem_mutex_leave(log_mutex);
 }
 
 void yaz_log_time_format(const char *fmt)
@@ -348,8 +352,8 @@ static char *clean_name(const char *name, int len, char *namebuf, int buflen)
 {
     char *p = namebuf;
     char *start = namebuf;
-    if (buflen <len)
-        len = buflen; 
+    if (buflen <= len)
+        len = buflen-1; 
     strncpy(namebuf, name, len);
     namebuf[len] = '\0';
     while ((p = strchr(start,'/')))
@@ -357,26 +361,32 @@ static char *clean_name(const char *name, int len, char *namebuf, int buflen)
     if ((p = strrchr(start,'.')))
         *p = '\0';
     return start;
-
 }
 
 static int define_module_bit(const char *name)
 {
     int i;
-    if (!mutex_init_flag)
-        init_mutex();
+    init_mutex();
+    nmem_mutex_enter(log_mutex);
     for (i = 0; mask_names[i].name; i++)
-        ;
+        if (0 == strcmp(mask_names[i].name, name))
+        {
+            nmem_mutex_leave(log_mutex);
+            return mask_names[i].mask;
+        }
     if ( (i>=MAX_MASK_NAMES) || (next_log_bit >= 1<<31 ))
     {
-        yaz_log(YLOG_WARN,"No more log bits left, not logging '%s'", name);
+        nmem_mutex_leave(log_mutex);
+        yaz_log(YLOG_WARN, "No more log bits left, not logging '%s'", name);
         return 0;
     }
     mask_names[i].mask = next_log_bit;
     next_log_bit = next_log_bit<<1;
-    mask_names[i].name = xstrdup(name);
+    mask_names[i].name = malloc (strlen(name)+1);
+    strcpy(mask_names[i].name, name);
     mask_names[i+1].name = NULL;
     mask_names[i+1].mask = 0;
+    nmem_mutex_leave(log_mutex);
     return mask_names[i].mask;
 }
 
@@ -385,18 +395,21 @@ int yaz_log_module_level(const char *name)
     int i;
     char clean[255];
     char *n = clean_name(name, strlen(name), clean, sizeof(clean));
-    if (!mutex_init_flag)
-        init_mutex();
+    init_mutex();
+    
+    nmem_mutex_enter(log_mutex);
     for (i = 0; mask_names[i].name; i++)
-        if (0==strcmp(n,mask_names[i].name))
+        if (0==strcmp(n, mask_names[i].name))
         {
-            yaz_log(YLOG_LOGLVL,"returning log bit 0x%x for '%s' %s",
+            nmem_mutex_leave(log_mutex);
+            yaz_log(YLOG_LOGLVL, "returning log bit 0x%x for '%s' %s",
                     mask_names[i].mask, n, 
-                    strcmp(n,name)?name:"" );
+                    strcmp(n,name) ? name : "");
             return mask_names[i].mask;
         }
-    yaz_log(YLOG_LOGLVL,"returning NO log bit for '%s' %s", n, 
-                    strcmp(n,name)?name:"" );
+    nmem_mutex_leave(log_mutex);
+    yaz_log(YLOG_LOGLVL, "returning NO log bit for '%s' %s", n, 
+                    strcmp(n, name) ? name : "" );
     return 0;
 }
 
@@ -408,44 +421,33 @@ int yaz_log_mask_str (const char *str)
 int yaz_log_mask_str_x (const char *str, int level)
 {
     const char *p;
-    int i;
-    char clean[255] = "";
-    char *n = clean;
 
     while (*str)
     {
-        int found = 0;
         int negated = 0;
         for (p = str; *p && *p != ','; p++)
             ;
         if (*str=='-')
         {
-            negated=1;
+            negated = 1;
             str++;
         }
         if (isdigit(*(unsigned char *) str))
         {
-            level = atoi (str);
-            found = 1;
+            level = atoi(str);
         }
         else 
         {
-            n = clean_name(str, p-str, clean, sizeof(clean));
-            for (i = 0; mask_names[i].name; i++)
-                if (0==strcmp (mask_names[i].name,n))
-                {
-                    if (mask_names[i].mask)
-                        if (negated)
-                            level &= ~mask_names[i].mask;
-                        else
-                            level |= mask_names[i].mask;
-                    else
-                        level = 0; /* 'none' clears them all */
-                    found = 1;
-                }
+            char clean[509];
+            char *n = clean_name(str, p-str, clean, sizeof(clean));
+            int mask = define_module_bit(n);
+            if (!mask)
+                level = 0;  /* 'none' clears them all */
+            else if (negated)
+                level &= ~mask;
+            else
+                level |= mask;
         }
-        if (!found)
-            level |= define_module_bit(n);
         if (*p == ',')
             p++;
         str = p;
