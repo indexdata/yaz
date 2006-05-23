aecf826f8ee83290a18c2c1415b55a46093c1a45
Adam Dickmeiss <adam@indexdata.dk>
2006-05-23 13:43:40 +0000

    Fixed bug #592: CVS version of YAZ require gnu make.

--- src/csvtobib1.tcl
+++ src/csvtobib1.tcl
@@ -3,16 +3,16 @@
 exec tclsh "$0" "$@"
 #
 # This file is part of the YAZ toolkit
-# Copyright (c) Index Data 1996-2005
+# Copyright (c) Index Data 1996-2006
 # See the file LICENSE for details.
 #
-# $Id: csvtobib1.tcl,v 1.1 2005-04-22 08:27:58 adam Exp $
+# $Id: csvtobib1.tcl,v 1.2 2006-05-23 13:43:40 adam Exp $
 #
 # Converts a CSV file with Bib-1 diagnostics to C+H file for easy
 # maintenance
 #
-# $Id: csvtobib1.tcl,v 1.1 2005-04-22 08:27:58 adam Exp $
+# $Id: csvtobib1.tcl,v 1.2 2006-05-23 13:43:40 adam Exp $
 
-source csvtodiag.tcl
+source [lindex $argv 0]/csvtodiag.tcl
 
-csvtodiag {bib1.csv diagbib1.c ../include/yaz/diagbib1.h} bib1 diagbib1_str
+csvtodiag [list [lindex $argv 0]/bib1.csv diagbib1.c ../include/yaz/diagbib1.h] bib1 diagbib1_str
--- src/csvtosrw.tcl
+++ src/csvtosrw.tcl
@@ -3,16 +3,16 @@
 exec tclsh "$0" "$@"
 #
 # This file is part of the YAZ toolkit
-# Copyright (c) Index Data 1996-2005
+# Copyright (c) Index Data 1996-2006
 # See the file LICENSE for details.
 #
-# $Id: csvtosrw.tcl,v 1.1 2005-04-22 08:27:58 adam Exp $
+# $Id: csvtosrw.tcl,v 1.2 2006-05-23 13:43:40 adam Exp $
 #
 # Converts a CSV file with SRW diagnostics to C+H file for easy
 # maintenance
 #
-# $Id: csvtosrw.tcl,v 1.1 2005-04-22 08:27:58 adam Exp $
+# $Id: csvtosrw.tcl,v 1.2 2006-05-23 13:43:40 adam Exp $
 
-source csvtodiag.tcl
+source [lindex $argv 0]/csvtodiag.tcl
 
-csvtodiag {srw.csv diagsrw.c ../include/yaz/diagsrw.h} srw {}
+csvtodiag [list [lindex $argv 0]/srw.csv diagsrw.c ../include/yaz/diagsrw.h] srw {}
