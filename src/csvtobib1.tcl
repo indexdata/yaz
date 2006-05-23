11b3a0d1c1be30f5052bf2eba788229df63f13b9
Adam Dickmeiss <adam@indexdata.dk>
2006-05-23 20:43:37 +0000

    Fix output dir for diagnostics headers

--- src/csvtobib1.tcl
+++ src/csvtobib1.tcl
@@ -6,13 +6,13 @@ exec tclsh "$0" "$@"
 # Copyright (c) Index Data 1996-2006
 # See the file LICENSE for details.
 #
-# $Id: csvtobib1.tcl,v 1.2 2006-05-23 13:43:40 adam Exp $
+# $Id: csvtobib1.tcl,v 1.3 2006-05-23 20:43:37 adam Exp $
 #
 # Converts a CSV file with Bib-1 diagnostics to C+H file for easy
 # maintenance
 #
-# $Id: csvtobib1.tcl,v 1.2 2006-05-23 13:43:40 adam Exp $
+# $Id: csvtobib1.tcl,v 1.3 2006-05-23 20:43:37 adam Exp $
 
 source [lindex $argv 0]/csvtodiag.tcl
 
-csvtodiag [list [lindex $argv 0]/bib1.csv diagbib1.c ../include/yaz/diagbib1.h] bib1 diagbib1_str
+csvtodiag [list [lindex $argv 0]/bib1.csv diagbib1.c [lindex $argv 0]/../include/yaz/diagbib1.h] bib1 diagbib1_str
--- src/csvtosrw.tcl
+++ src/csvtosrw.tcl
@@ -6,13 +6,13 @@ exec tclsh "$0" "$@"
 # Copyright (c) Index Data 1996-2006
 # See the file LICENSE for details.
 #
-# $Id: csvtosrw.tcl,v 1.2 2006-05-23 13:43:40 adam Exp $
+# $Id: csvtosrw.tcl,v 1.3 2006-05-23 20:43:37 adam Exp $
 #
 # Converts a CSV file with SRW diagnostics to C+H file for easy
 # maintenance
 #
-# $Id: csvtosrw.tcl,v 1.2 2006-05-23 13:43:40 adam Exp $
+# $Id: csvtosrw.tcl,v 1.3 2006-05-23 20:43:37 adam Exp $
 
 source [lindex $argv 0]/csvtodiag.tcl
 
-csvtodiag [list [lindex $argv 0]/srw.csv diagsrw.c ../include/yaz/diagsrw.h] srw {}
+csvtodiag [list [lindex $argv 0]/srw.csv diagsrw.c [lindex $argv 0]/../include/yaz/diagsrw.h] srw {}
