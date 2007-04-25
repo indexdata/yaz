544ac779d4c6b5d3d07f54486a7cb851bd6f74ea
Adam Dickmeiss <adam@indexdata.dk>
2007-04-25 20:50:56 +0000

    Removed ccl_pr_tree_as_qrpn . Implement ccl_pr_tree in terms of ccl_pquery_indent. Saves roughly 50 lines of code (DRY).

--- src/cclptree.c
+++ src/cclptree.c
@@ -53,7 +53,7 @@
 /* CCL print rpn tree - infix notation
  * Europagate, 1995
  *
- * $Id: cclptree.c,v 1.8 2007-03-19 22:17:41 adam Exp $
+ * $Id: cclptree.c,v 1.9 2007-04-25 20:50:56 adam Exp $
  *
  * Old Europagate Log:
  *
@@ -83,110 +83,22 @@
 
 #include <yaz/ccl.h>
 
-void fprintSpaces(int indent,FILE * fd_out) 
+static void ccl_pquery_indent(WRBUF w, struct ccl_rpn_node *p, int indent);
+
+static void ccl_pquery_complex(WRBUF w, struct ccl_rpn_node *p, int indent)
 {
-    char buf[100];
-    sprintf(buf,"%%%d.s",indent);
-    fprintf(fd_out,buf," ");
-}
-
-void ccl_pr_tree_as_qrpn(struct ccl_rpn_node *rpn, FILE *fd_out, int indent)
-{
-    if(indent>0)
-        fprintSpaces(indent,fd_out);
-    switch (rpn->kind)
-    {
-    case CCL_RPN_TERM:
-        if (rpn->u.t.attr_list)
-        {
-            struct ccl_rpn_attr *attr;
-            for (attr = rpn->u.t.attr_list; attr; attr = attr->next)
-            {
-                if (attr->set)
-                    fprintf(fd_out, "@attr %s ", attr->set);
-                else
-                    fprintf(fd_out, "@attr ");
-                switch(attr->kind)
-                {
-                case CCL_RPN_ATTR_NUMERIC:
-                    fprintf (fd_out, "%d=%d ", attr->type,
-                             attr->value.numeric);
-                    break;
-                case CCL_RPN_ATTR_STRING:
-                    fprintf (fd_out, "%d=%s ", attr->type,
-                             attr->value.str);
-                }
-            }
-        }
-        fprintf (fd_out, "\"%s\"\n", rpn->u.t.term);
-        break;
-    case CCL_RPN_AND:
-        fprintf (fd_out, "@and \n");
-        ccl_pr_tree_as_qrpn (rpn->u.p[0], fd_out,indent+2);
-        ccl_pr_tree_as_qrpn (rpn->u.p[1], fd_out,indent+2);
-        break;
-    case CCL_RPN_OR:
-        fprintf (fd_out, "@or \n");
-        ccl_pr_tree_as_qrpn (rpn->u.p[0], fd_out,indent+2);
-        ccl_pr_tree_as_qrpn (rpn->u.p[1], fd_out,indent+2);
-        break;
-    case CCL_RPN_NOT:
-        fprintf (fd_out, "@not ");
-        ccl_pr_tree_as_qrpn (rpn->u.p[0], fd_out,indent+2);
-        ccl_pr_tree_as_qrpn (rpn->u.p[1], fd_out,indent+2);
-        break;
-    case CCL_RPN_SET:
-        fprintf (fd_out, "set=%s ", rpn->u.setname);
-        break;
-    case CCL_RPN_PROX:
-        if (rpn->u.p[2] && rpn->u.p[2]->kind == CCL_RPN_TERM)
-        {
-            const char *cp = rpn->u.p[2]->u.t.term;
-            /* exlusion distance ordered relation which-code unit-code */
-            if (*cp == '!')
-            {   
-                /* word order specified */
-                if (isdigit(((const unsigned char *) cp)[1]))
-                    fprintf(fd_out, "@prox 0 %s 1 2 known 2", cp+1);
-                else
-                    fprintf(fd_out, "@prox 0 1 1 2 known 2");
-            } 
-            else if (*cp == '%')
-            {
-                /* word order not specified */
-                if (isdigit(((const unsigned char *) cp)[1]))
-                    fprintf(fd_out, "@prox 0 %s 0 2 known 2", cp+1);
-                else
-                    fprintf(fd_out, "@prox 0 1 0 2 known 2");
-            }
-        }
-        ccl_pr_tree_as_qrpn (rpn->u.p[0], fd_out,indent+2);
-        ccl_pr_tree_as_qrpn (rpn->u.p[1], fd_out,indent+2);
-        break;
-    default:
-        fprintf(stderr,"Internal Error Unknown ccl_rpn node type %d\n",rpn->kind);
-    }
-}
-
-
-void ccl_pr_tree (struct ccl_rpn_node *rpn, FILE *fd_out)
-{
-    ccl_pr_tree_as_qrpn(rpn,fd_out,0);
-}
-
-
-static void ccl_pquery_complex (WRBUF w, struct ccl_rpn_node *p)
-{
+    int sep_char = indent == -1 ? ' ' : '\n';
+    int next_indent = indent == -1 ? indent : indent+1;
     switch (p->kind)
     {
     case CCL_RPN_AND:
-        wrbuf_puts(w, "@and ");
+        wrbuf_puts(w, "@and");
         break;
     case CCL_RPN_OR:
-        wrbuf_puts(w, "@or ");
+        wrbuf_puts(w, "@or");
         break;
     case CCL_RPN_NOT:
-        wrbuf_puts(w, "@not ");
+        wrbuf_puts(w, "@not");
         break;
     case CCL_RPN_PROX:
         if (p->u.p[2] && p->u.p[2]->kind == CCL_RPN_TERM)
@@ -197,81 +109,112 @@ static void ccl_pquery_complex (WRBUF w, struct ccl_rpn_node *p)
             {   
                 /* word order specified */
                 if (isdigit(((const unsigned char *) cp)[1]))
-                    wrbuf_printf(w, "@prox 0 %s 1 2 k 2 ", cp+1);
+                    wrbuf_printf(w, "@prox 0 %s 1 2 k 2", cp+1);
                 else
-                    wrbuf_printf(w, "@prox 0 1 1 2 k 2 ");
+                    wrbuf_printf(w, "@prox 0 1 1 2 k 2");
             } 
             else if (*cp == '%')
             {
                 /* word order not specified */
                 if (isdigit(((const unsigned char *) cp)[1]))
-                    wrbuf_printf(w, "@prox 0 %s 0 2 k 2 ", cp+1);
+                    wrbuf_printf(w, "@prox 0 %s 0 2 k 2", cp+1);
                 else
-                    wrbuf_printf(w, "@prox 0 1 0 2 k 2 ");
+                    wrbuf_printf(w, "@prox 0 1 0 2 k 2");
             }
         }
         else
-            wrbuf_puts(w, "@prox 0 2 0 1 k 2 ");
+            wrbuf_puts(w, "@prox 0 2 0 1 k 2");
         break;
     default:
-        wrbuf_puts(w, "@ bad op (unknown) ");
+        wrbuf_puts(w, "@ bad op (unknown)");
     }
-    ccl_pquery(w, p->u.p[0]);
-    ccl_pquery(w, p->u.p[1]);
+    wrbuf_putc(w, sep_char);
+    ccl_pquery_indent(w, p->u.p[0], next_indent);
+    ccl_pquery_indent(w, p->u.p[1], next_indent);
 }
 
-void ccl_pquery (WRBUF w, struct ccl_rpn_node *p)
+static void ccl_prterm(WRBUF w, const char *term)
+{
+    const char *cp = term;
+    for (; *cp; cp++)
+    {
+        if (*cp == ' ' || *cp == '\\')
+            wrbuf_putc(w, '\\');
+        wrbuf_putc(w, *cp);
+    }
+    wrbuf_puts(w, " ");
+}
+
+static void ccl_pquery_indent(WRBUF w, struct ccl_rpn_node *p, int indent)
 {
     struct ccl_rpn_attr *att;
-    const char *cp;
-        
+
+    if (indent != -1)
+    {
+        int i;
+        for (i = 0; i < indent; i++)
+            wrbuf_putc(w, ' ');
+    }
     switch (p->kind)
     {
     case CCL_RPN_AND:
     case CCL_RPN_OR:
     case CCL_RPN_NOT:
     case CCL_RPN_PROX:
-        ccl_pquery_complex (w, p);
+        ccl_pquery_complex(w, p, indent);
         break;
     case CCL_RPN_SET:
-        wrbuf_puts (w, "@set ");
-        wrbuf_puts (w, p->u.setname);
-        wrbuf_puts (w, " ");
+        wrbuf_puts(w, "@set ");
+        ccl_prterm(w, p->u.setname);
+        if (indent != -1)
+            wrbuf_putc(w, '\n');
         break;
     case CCL_RPN_TERM:
         for (att = p->u.t.attr_list; att; att = att->next)
         {
             char tmpattr[128];
-            wrbuf_puts (w, "@attr ");
+            wrbuf_puts(w, "@attr ");
             if (att->set)
             {
-                wrbuf_puts (w, att->set);
-                wrbuf_puts (w, " ");
+                wrbuf_puts(w, att->set);
+                wrbuf_puts(w, " ");
             }
             switch(att->kind)
             {
             case CCL_RPN_ATTR_NUMERIC:
                 sprintf(tmpattr, "%d=%d ", att->type, att->value.numeric);
-                wrbuf_puts (w, tmpattr);
+                wrbuf_puts(w, tmpattr);
                 break;
             case CCL_RPN_ATTR_STRING:
                 sprintf(tmpattr, "%d=", att->type);
-                wrbuf_puts (w, tmpattr);
+                wrbuf_puts(w, tmpattr);
                 wrbuf_puts(w, att->value.str);
-                wrbuf_puts (w, " ");
+                wrbuf_puts(w, " ");
                 break;
             }
         }
-        for (cp = p->u.t.term; *cp; cp++)
-        {
-            if (*cp == ' ' || *cp == '\\')
-                wrbuf_putc (w, '\\');
-            wrbuf_putc (w, *cp);
-        }
-        wrbuf_puts (w, " ");
+        ccl_prterm(w, p->u.t.term);
+        if (indent != -1)
+            wrbuf_putc(w, '\n');
         break;
     }
 }
+
+void ccl_pquery(WRBUF w, struct ccl_rpn_node *p)
+{
+    ccl_pquery_indent(w, p, -1);
+}
+
+void ccl_pr_tree(struct ccl_rpn_node *rpn, FILE *fd_out)
+{
+    WRBUF w = wrbuf_alloc();
+
+    ccl_pquery_indent(w, rpn, 0);
+    
+    fputs(wrbuf_cstr(w), fd_out);
+    wrbuf_destroy(w);
+}
+
 /*
  * Local variables:
  * c-basic-offset: 4
