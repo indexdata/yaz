8fa6874f7c143499d03c5fdcfbc50bd01a31a58c
Adam Dickmeiss <adam@indexdata.dk>
2007-05-16 10:47:14 +0000

    Bump to version 3.0.3

--- include/yaz/yaz-version.h
+++ include/yaz/yaz-version.h
@@ -24,7 +24,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-/* $Id: yaz-version.h,v 1.123 2007-05-08 08:27:14 adam Exp $ */
+/* $Id: yaz-version.h,v 1.124 2007-05-16 10:47:14 adam Exp $ */
 /**
  * \file yaz-version.h
  * \brief Defines YAZ version.
@@ -33,8 +33,8 @@
 
 #include <yaz/yconfig.h>
 
-#define YAZ_VERSION "3.0.2"
-#define YAZ_VERSIONL 0x030002
+#define YAZ_VERSION "3.0.3"
+#define YAZ_VERSIONL 0x030003
 
 #define YAZ_DATE 1
 
