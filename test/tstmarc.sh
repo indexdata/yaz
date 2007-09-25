185d51016579faad34f8b62151ee1d5125a4d430
Adam Dickmeiss <adam@indexdata.dk>
2007-09-25 07:34:03 +0000

    Combine test from tstmarciso.sh and tstmarcxml.sh to tstmarc.sh .. Mostly because the latter requires output from the former (so make -j2 could break).

--- test/tstmarc.sh
+++ test/tstmarc.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-# $Id: tstmarciso.sh,v 1.5 2007-01-18 11:44:50 adam Exp $
+# $Id: tstmarc.sh,v 1.4 2007-09-25 07:34:03 adam Exp $
 # Tests reading of ISO2709 and checks that we get identical MARCXML
 # 
-# Reads marc?.marc files , Generates marc?.xml files
+# Reads marc?.marc files , Generates marc*.xml files
+# If Libxml2 is present, the marc*.xml files are parsed again..
 srcdir=${srcdir:-.}
 ecode=0
-for f in ${srcdir}/marc?.marc; do
+
+../util/yaz-marcdump -i marcxml >/dev/null 2>&1
+if test $? = "3"; then
+    noxml=1
+fi
+
+for f in ${srcdir}/marc[0-9].marc; do
     fb=`basename ${f} .marc`
     CHR=${srcdir}/${fb}.chr
     NEW=${fb}.new.xml
@@ -38,6 +45,30 @@ for f in ${srcdir}/marc?.marc; do
 	    ecode=1
 	fi
     fi
+
+    if test -z "$noxml"; then
+	f=$OLD
+	OLD=${f}.marc
+	NEW=`basename ${f}`.new.marc
+	DIFF=`basename ${f}`.diff
+	../util/yaz-marcdump -f utf-8 -t utf-8 -i marcxml -o marc $f > $NEW
+	if test $? != "0"; then
+	    echo "Failed decode of $f"
+	    ecode=1
+	elif test -f $OLD; then
+	    if diff $OLD $NEW >$DIFF; then
+		rm $DIFF
+		rm $NEW
+	    else
+		echo "$f: $NEW and $OLD Differ"
+		ecode=1
+	    fi
+	else
+	    echo "$f: Making test result $OLD for the first time"
+	    mv $NEW $OLD
+	fi
+    fi
+    
 done
 exit $ecode
 
