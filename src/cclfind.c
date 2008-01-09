65371f5d5f265a4c5ee43f784b2d8a6db1e0774a
Adam Dickmeiss <adam@indexdata.dk>
2008-01-09 21:32:27 +0000

    Changed license of CCL module to 'Revised BSD'.

--- src/cclfind.c
+++ src/cclfind.c
@@ -1,47 +1,9 @@
 /*
- * Copyright (c) 1995, the EUROPAGATE consortium (see below).
+ * Copyright (C) 1995-2008, Index Data ApS
+ * See the file LICENSE for details.
  *
- * The EUROPAGATE consortium members are:
- *
- *    University College Dublin
- *    Danmarks Teknologiske Videnscenter
- *    An Chomhairle Leabharlanna
- *    Consejo Superior de Investigaciones Cientificas
- *
- * Permission to use, copy, modify, distribute, and sell this software and
- * its documentation, in whole or in part, for any purpose, is hereby granted,
- * provided that:
- *
- * 1. This copyright and permission notice appear in all copies of the
- * software and its documentation. Notices of copyright or attribution
- * which appear at the beginning of any file must remain unchanged.
- *
- * 2. The names of EUROPAGATE or the project partners may not be used to
- * endorse or promote products derived from this software without specific
- * prior written permission.
- *
- * 3. Users of this software (implementors and gateway operators) agree to
- * inform the EUROPAGATE consortium of their use of the software. This
- * information will be used to evaluate the EUROPAGATE project and the
- * software, and to plan further developments. The consortium may use
- * the information in later publications.
- * 
- * 4. Users of this software agree to make their best efforts, when
- * documenting their use of the software, to acknowledge the EUROPAGATE
- * consortium, and the role played by the software in their work.
- *
- * THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED, OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL THE EUROPAGATE CONSORTIUM OR ITS MEMBERS BE LIABLE
- * FOR ANY SPECIAL, INCIDENTAL, INDIRECT OR CONSEQUENTIAL DAMAGES OF
- * ANY KIND, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA
- * OR PROFITS, WHETHER OR NOT ADVISED OF THE POSSIBILITY OF DAMAGE, AND
- * ON ANY THEORY OF LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE
- * USE OR PERFORMANCE OF THIS SOFTWARE.
- *
+ * $Id: cclfind.c,v 1.15 2008-01-09 21:32:27 adam Exp $
  */
-
 /** 
  * \file cclfind.c
  * \brief Implements parsing of a CCL FIND query.
@@ -50,64 +12,6 @@
  * The parser uses predictive parsing, but it does several tokens
  * of lookahead in the handling of relational operations.. So
  * it's not really pure.
- */
-
-
-/* CCL find (to rpn conversion)
- * Europagate, 1995
- *
- * $Id: cclfind.c,v 1.14 2007-05-01 12:22:11 adam Exp $
- *
- * Old Europagate log:
- *
- * Revision 1.16  1996/01/08  08:41:13  adam
- * Removed unused function.
- *
- * Revision 1.15  1995/07/20  08:14:34  adam
- * Qualifiers were observed too often. Instead tokens are treated as
- * qualifiers only when separated by comma.
- *
- * Revision 1.14  1995/05/16  09:39:26  adam
- * LICENSE.
- *
- * Revision 1.13  1995/04/17  09:31:42  adam
- * Improved handling of qualifiers. Aliases or reserved words.
- *
- * Revision 1.12  1995/03/20  15:27:43  adam
- * Minor changes.
- *
- * Revision 1.11  1995/02/23  08:31:59  adam
- * Changed header.
- *
- * Revision 1.9  1995/02/16  13:20:06  adam
- * Spell fix.
- *
- * Revision 1.8  1995/02/14  19:59:42  adam
- * Removed a syntax error.
- *
- * Revision 1.7  1995/02/14  19:55:10  adam
- * Header files ccl.h/cclp.h are gone! They have been merged an
- * moved to ../include/ccl.h.
- * Node kind(s) in ccl_rpn_node have changed names.
- *
- * Revision 1.6  1995/02/14  16:20:55  adam
- * Qualifiers are read from a file now.
- *
- * Revision 1.5  1995/02/14  14:12:41  adam
- * Ranges for ordered qualfiers implemented (e.g. pd=1980-1990).
- *
- * Revision 1.4  1995/02/14  13:16:29  adam
- * Left and/or right truncation implemented.
- *
- * Revision 1.3  1995/02/14  10:25:56  adam
- * The constructions 'qualifier rel term ...' implemented.
- *
- * Revision 1.2  1995/02/13  15:15:07  adam
- * Added handling of qualifiers. Not finished yet.
- *
- * Revision 1.1  1995/02/13  12:35:20  adam
- * First version of CCL. Qualifiers aren't handled yet.
- *
  */
 
 #include <stdlib.h>
--- src/cclstr.c
+++ src/cclstr.c
@@ -1,68 +1,12 @@
 /*
- * Copyright (c) 1995, the EUROPAGATE consortium (see below).
+ * Copyright (C) 1995-2008, Index Data ApS
+ * See the file LICENSE for details.
  *
- * The EUROPAGATE consortium members are:
- *
- *    University College Dublin
- *    Danmarks Teknologiske Videnscenter
- *    An Chomhairle Leabharlanna
- *    Consejo Superior de Investigaciones Cientificas
- *
- * Permission to use, copy, modify, distribute, and sell this software and
- * its documentation, in whole or in part, for any purpose, is hereby granted,
- * provided that:
- *
- * 1. This copyright and permission notice appear in all copies of the
- * software and its documentation. Notices of copyright or attribution
- * which appear at the beginning of any file must remain unchanged.
- *
- * 2. The names of EUROPAGATE or the project partners may not be used to
- * endorse or promote products derived from this software without specific
- * prior written permission.
- *
- * 3. Users of this software (implementors and gateway operators) agree to
- * inform the EUROPAGATE consortium of their use of the software. This
- * information will be used to evaluate the EUROPAGATE project and the
- * software, and to plan further developments. The consortium may use
- * the information in later publications.
- * 
- * 4. Users of this software agree to make their best efforts, when
- * documenting their use of the software, to acknowledge the EUROPAGATE
- * consortium, and the role played by the software in their work.
- *
- * THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED, OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL THE EUROPAGATE CONSORTIUM OR ITS MEMBERS BE LIABLE
- * FOR ANY SPECIAL, INCIDENTAL, INDIRECT OR CONSEQUENTIAL DAMAGES OF
- * ANY KIND, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA
- * OR PROFITS, WHETHER OR NOT ADVISED OF THE POSSIBILITY OF DAMAGE, AND
- * ON ANY THEORY OF LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE
- * USE OR PERFORMANCE OF THIS SOFTWARE.
- *
+ * $Id: cclstr.c,v 1.4 2008-01-09 21:32:28 adam Exp $
  */
 /** 
  * \file cclstr.c
  * \brief Implements CCL string compare utilities
- */
-/* CCL string compare utilities
- * Europagate, 1995
- *
- * $Id: cclstr.c,v 1.3 2005-06-25 15:46:03 adam Exp $
- *
- * Old Europagate Log:
- *
- * Revision 1.3  1996/01/24  10:11:19  adam
- * Added include of stdlib.h.
- *
- * Revision 1.2  1995/05/16  09:39:27  adam
- * LICENSE.
- *
- * Revision 1.1  1995/05/11  14:03:57  adam
- * Changes in the reading of qualifier(s). New function: ccl_qual_fitem.
- * New variable ccl_case_sensitive, which controls whether reserved
- * words and field names are case sensitive or not.
- *
  */
 #include <ctype.h>
 #include <stdio.h>
