8007b02f1f71539935da4895cea16fac3422e64c
Dennis Schafroth <dennis@indexdata.com>
2009-11-04 13:55:24 +0100

    Added break in reverse/'r' option check, or it would scan for iteration number.

--- util/cql2pqf.c
+++ util/cql2pqf.c
@@ -39,6 +39,7 @@ int main(int argc, char **argv)
             break;
         case 'r':
             reverse = 1;
+            break;
         case 'n':
             iterations = atoi(arg);
             break;
