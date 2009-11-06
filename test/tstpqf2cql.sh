69c2dee55aa204cab368989b8385eda8b83bf2e2
Dennis Schafroth <dennis@indexdata.com>
2009-11-06 11:38:38 +0100

    Added tests for pql2cql using relation and truncation.

---
+++ test/tstpqf2cql.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+srcdir=${srcdir:-.}
+oIFS="$IFS"
+IFS='
+'
+secno=0
+testno=0
+comment=0
+ecode=0
+test -f ${srcdir}/pqf2cqlsample || exit 1
+test -d pqf2cql || mkdir pqf2cql
+for f in `cat ${srcdir}/pqf2cqlsample`; do
+	if echo $f | grep '^#' >/dev/null; then
+		comment=1
+	else
+		if test "$comment" = "1"; then
+			secno=`expr $secno + 1`
+			testno=0
+		fi
+		comment=0
+		testno=`expr $testno + 1`
+		OUT1=${srcdir}/pqf2cql/$secno.$testno.out
+		ERR1=${srcdir}/pqf2cql/$secno.$testno.err
+		OUT2=pqf2cql/$secno.$testno.out.tmp
+		ERR2=pqf2cql/$secno.$testno.err.tmp
+		DIFF=pqf2cql/$secno.$testno.diff
+		../util/cql2pqf -r - "$f" >$OUT2 2>$ERR2
+		if test -f $OUT1 -a -f $ERR1; then
+			if diff $OUT1 $OUT2 >$DIFF; then
+				rm $DIFF
+				rm $OUT2
+			else
+				echo "diff out $secno $testno $f"
+				cat $DIFF
+				ecode=1	
+			fi
+			if diff $ERR1 $ERR2 >$DIFF; then
+				rm $DIFF
+				rm $ERR2
+			else
+				echo "diff err $secno $testno $f"
+				cat $DIFF
+				ecode=1
+			fi
+		else
+			echo "making test $secno $testno $f"
+			mv $OUT2 $OUT1
+			mv $ERR2 $ERR1
+			ecode=1
+		fi	
+	fi		
+done
+IFS="$oIFS"
+exit $ecode
