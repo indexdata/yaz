10c562bc3ccaaa440189f279ca562327612f87c6
Adam Dickmeiss <adam@indexdata.dk>
2009-11-26 09:49:58 +0100

    Dont enable 64-bit on Windows

--- include/yaz/nmem.h
+++ include/yaz/nmem.h
@@ -43,8 +43,14 @@ YAZ_BEGIN_CDECL
 /** \brief NMEM handle (an opaque pointer to memory) */
 typedef struct nmem_control *NMEM;
 
+#ifdef _MSC_VER
+#define NMEM_64 0
+#endif
+
 /** \brief Set to 1 if YAZ BER integer is 64-bit ; 0 otherwise */
+#ifndef NMEM_64
 #define NMEM_64 1
+#endif
 
 #if NMEM_64
 /** \brief BER/utility integer (64-bit or more) */
