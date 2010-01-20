8a6e1b377726cca5f60ce249b1a5bfcb6616bdab
Adam Dickmeiss <adam@indexdata.dk>
2010-01-20 15:05:19 +0100

    Fix JSON parser WRT start symbol
    The start grammar symbol (referred to as JSONText in ECMA-262) is
    JSONValue, not JSONObject.

--- test/tst_json.c
+++ test/tst_json.c
@@ -49,9 +49,9 @@ static void tst1(void)
 
     YAZ_CHECK(expect(p, "", 0));
 
-    YAZ_CHECK(expect(p, "1234", 0));
+    YAZ_CHECK(expect(p, "1234", "1234"));
 
-    YAZ_CHECK(expect(p, "[ 1234 ]", 0));
+    YAZ_CHECK(expect(p, "[ 1234 ]", "[1234]"));
 
     YAZ_CHECK(expect(p, "{\"k\":tru}", 0));
 
