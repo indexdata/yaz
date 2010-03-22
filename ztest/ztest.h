50680214cefa3d57c9e49db3524c4d5497d99365
Adam Dickmeiss <adam@indexdata.dk>
2010-03-22 15:39:31 +0100

    Reformat

--- ztest/ztest.h
+++ ztest/ztest.h
@@ -31,9 +31,9 @@
 
 #include <yaz/proto.h>
 
-Z_GenericRecord *dummy_grs_record (int num, ODR o);
-char *dummy_marc_record (int num, ODR odr);
-char *dummy_xml_record (int num, ODR odr);
+Z_GenericRecord *dummy_grs_record(int num, ODR o);
+char *dummy_marc_record(int num, ODR odr);
+char *dummy_xml_record(int num, ODR odr);
 Z_OPACRecord *dummy_opac(int num, ODR odr, const char *marc_input);
 
 /*
