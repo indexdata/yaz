1064b0521a608ba2c985f893772315819a990372
Dennis Schafroth <dennis@indexdata.com>
2010-11-08 16:25:23 +0100

    Add Stemming API (following ICU transforms)

---
+++ src/stemmer.c
@@ -0,0 +1,33 @@
+
+
+
+#include <yaz/stemmer.h>
+
+#include <unicode/ustring.h>  /* some more string fcns*/
+#include <unicode/uchar.h>    /* char names           */
+
+struct yaz_stemmer_t
+{
+    int implementation;
+    union {
+        struct sb_stemmer *snowballer;
+    };
+};
+
+yaz_stemmer_p yaz_stemmer_create(const char *locale, const char *rule, UErrorCode *status) {
+    return 0;
+}
+
+yaz_stemmer_p yaz_stemmer_clone(yaz_stemmer_p stemmer) {
+    return 0;
+}
+
+void yaz_stemmer_stem(yaz_stemmer_p stemmer, struct icu_buf_utf16 *dst, struct icu_buf_utf16* src, UErrorCode *status) {
+
+}
+
+void yaz_stemmer_destroy(yaz_stemmer_p stemmer) {
+
+
+}
+
