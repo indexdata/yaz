4aaca71aaade2a94eab1806a11967d672e7c90dd
Adam Dickmeiss <adam@indexdata.dk>
2011-08-03 11:40:33 +0200

    ZOOM: addinfo includes HTTP content when SRU decode fails
    When ZOOM C sees an SRU responst that it cannot decode it
    returns "meta" diagnostic ZOOM_ERROR_DECODE/10003 . The additional
    info now contains the HTTP content.

--- src/zoom-c.c
+++ src/zoom-c.c
@@ -1563,7 +1563,7 @@ static void handle_http(ZOOM_connection c, Z_HTTP_Response *hres)
     }
     else 
     {  
-        ret = ZOOM_handle_sru(c, hres, &cret);
+        ret = ZOOM_handle_sru(c, hres, &cret, &addinfo);
         if (ret == 0)
         {
             if (c->no_redirects) /* end of redirect. change hosts again */
@@ -1576,7 +1576,11 @@ static void handle_http(ZOOM_connection c, Z_HTTP_Response *hres)
         if (hres->code != 200)
             ZOOM_set_HTTP_error(c, hres->code, 0, 0);
         else
+        {
+            yaz_log(YLOG_LOG, "set error... addinfo=%s", addinfo ?
+                    addinfo : "NULL");
             ZOOM_set_error(c, ZOOM_ERROR_DECODE, addinfo);
+        }
         ZOOM_connection_close(c);
     }
     if (cret == zoom_complete)
