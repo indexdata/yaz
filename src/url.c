8cd8912e4ff99fd53ff78a028f7d84418e494b0d
Adam Dickmeiss <adam@indexdata.dk>
2011-08-10 15:03:53 +0200

    New utility yaz_url: fetches HTTP content
    This is a simple wrapper around COMSTACK and HTTP utilities; deals
    with proxy'ing.

---
+++ src/url.c
@@ -0,0 +1,156 @@
+/* This file is part of the YAZ toolkit.
+ * Copyright (C) 1995-2011 Index Data
+ * See the file LICENSE for details.
+ */
+/**
+ * \file url.c
+ * \brief URL fetch utility
+ */
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <yaz/url.h>
+#include <yaz/comstack.h>
+#include <yaz/log.h>
+
+struct yaz_url {
+    ODR odr_in;
+    ODR odr_out;
+    char *proxy;
+};
+
+yaz_url_t yaz_url_create(void)
+{
+    yaz_url_t p = xmalloc(sizeof(*p));
+    p->odr_in = odr_createmem(ODR_DECODE);
+    p->odr_out = odr_createmem(ODR_ENCODE);
+    p->proxy = 0;
+    return p;
+}
+
+void yaz_url_destroy(yaz_url_t p)
+{
+    if (p)
+    {
+        odr_destroy(p->odr_in);
+        odr_destroy(p->odr_out);
+        xfree(p->proxy);
+        xfree(p);
+    }
+}
+
+void yaz_url_set_proxy(yaz_url_t p, const char *proxy)
+{
+    xfree(p->proxy);
+    p->proxy = 0;
+    if (proxy && *proxy)
+        p->proxy = xstrdup(proxy);
+}
+
+Z_HTTP_Response *yaz_url_exec(yaz_url_t p, const char *uri,
+                              const char *method,
+                              Z_HTTP_Header *headers,
+                              const char *buf, size_t len)
+{
+    Z_HTTP_Response *res = 0;
+    int number_of_redirects = 0;
+
+    while (1)
+    {
+        void *add;
+        COMSTACK conn = 0;
+        int code;
+        struct Z_HTTP_Header **last_header_entry;
+        const char *location = 0;
+        Z_GDU *gdu = z_get_HTTP_Request_uri(p->odr_out, uri, 0,
+                                            p->proxy ? 1 : 0);
+        gdu->u.HTTP_Request->method = odr_strdup(p->odr_out, method);
+
+        res = 0;
+        last_header_entry = &gdu->u.HTTP_Request->headers;
+        while (*last_header_entry)
+            last_header_entry = &(*last_header_entry)->next;
+        *last_header_entry = headers; /* attach user headers */
+
+        if (buf && len)
+        {
+            gdu->u.HTTP_Request->content_buf = (char *) buf;
+            gdu->u.HTTP_Request->content_len = len;
+        }
+        if (!z_GDU(p->odr_out, &gdu, 0, 0))
+        {
+            yaz_log(YLOG_WARN, "Can not encode HTTP request URL:%s", uri);
+            return 0;
+        }
+        conn = cs_create_host_proxy(uri, 1, &add, p->proxy);
+        if (!conn)
+        {
+            yaz_log(YLOG_WARN, "Bad address for URL:%s", uri);
+        }
+        else if (cs_connect(conn, add) < 0)
+        {
+            yaz_log(YLOG_WARN, "Can not connect to URL:%s", uri);
+        }
+        else
+        {
+            int len;
+            char *buf = odr_getbuf(p->odr_out, &len, 0);
+            
+            if (cs_put(conn, buf, len) < 0)
+                yaz_log(YLOG_WARN, "cs_put failed URL:%s", uri);
+            else
+            {
+                char *netbuffer = 0;
+                int netlen = 0;
+                int cs_res = cs_get(conn, &netbuffer, &netlen);
+                if (cs_res <= 0)
+                {
+                    yaz_log(YLOG_WARN, "cs_get failed URL:%s", uri);
+                }
+                else
+                {
+                    Z_GDU *gdu;
+                    odr_setbuf(p->odr_in, netbuffer, cs_res, 0);
+                    if (!z_GDU(p->odr_in, &gdu, 0, 0)
+                        || gdu->which != Z_GDU_HTTP_Response)
+                    {
+                        yaz_log(YLOG_WARN, "HTTP decoding failed "
+                                "URL:%s", uri);
+                    }
+                    else
+                    {
+                        res = gdu->u.HTTP_Response;
+                    }
+                }
+                xfree(netbuffer);
+            }
+        }
+        if (conn)
+            cs_close(conn);
+        if (!res)
+            break;
+        code = res->code;
+        location = z_HTTP_header_lookup(res->headers, "Location");
+        if (++number_of_redirects < 10 &&
+            location && (code == 301 || code == 302 || code == 307))
+        {
+            odr_reset(p->odr_out);
+            uri = odr_strdup(p->odr_out, location);
+            odr_reset(p->odr_in);
+        }
+        else
+            break;
+    }
+    return res;
+}
+
+/*
+ * Local variables:
+ * c-basic-offset: 4
+ * c-file-style: "Stroustrup"
+ * indent-tabs-mode: nil
+ * End:
+ * vim: shiftwidth=4 tabstop=8 expandtab
+ */
+
---
+++ util/yaz-url.c
@@ -0,0 +1,136 @@
+/* This file is part of the YAZ toolkit.
+ * Copyright (C) 1995-2011 Index Data
+ * See the file LICENSE for details.
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <yaz/url.h>
+#include <yaz/options.h>
+#include <yaz/log.h>
+
+static void usage(void)
+{
+    printf("yaz-icu [options] url ..\n");
+    printf(" -H name=value       HTTP header\n");
+    printf(" -p file             POST content of file\n");
+    printf(" -u user/password    Basic HTTP auth\n");
+    printf(" -x proxy            HTTP proxy\n");
+    exit(1);
+}
+
+static char *get_file(const char *fname, size_t *len)
+{
+    char *buf = 0;
+    FILE *inf = fopen(fname, "rb");
+    if (!inf)
+    {
+        yaz_log(YLOG_FATAL|YLOG_ERRNO, "Could not open %s", fname);
+        exit(1);
+    }
+    if (fseek(inf, 0L, SEEK_END))
+    {
+        yaz_log(YLOG_FATAL|YLOG_ERRNO, "fseek of %s failed", fname);
+        exit(1);
+    }
+    *len = ftell(inf);
+    buf = xmalloc(*len);
+    fseek(inf, 0L, SEEK_SET);
+    fread(buf, 1, *len, inf);
+    fclose(inf);
+    return buf;
+}
+
+int main(int argc, char **argv)
+{
+    int ret;
+    char *arg;
+    yaz_url_t p = yaz_url_create();
+    char *post_buf = 0;
+    size_t post_len = 0;
+    const char *method = "GET";
+    Z_HTTP_Response *http_response;
+    Z_HTTP_Header *http_headers = 0;
+    ODR odr = odr_createmem(ODR_ENCODE);
+    int exit_code = 0;
+
+    while ((ret = options("hH:p:u:x:", argv, argc, &arg))
+           != YAZ_OPTIONS_EOF)
+    {
+        switch (ret)
+        {
+        case 'h':
+            usage();
+            break;
+        case 'H':
+            if (!strchr(arg, '='))
+            {
+                yaz_log(YLOG_FATAL, "bad header option (missing =): %s\n", arg);
+                exit_code = 1;
+            }
+            else
+            {
+                char *cp = strchr(arg, '=');
+                char *name = odr_malloc(odr, 1 + cp - arg);
+                char *value = cp + 1;
+                memcpy(name, arg, cp - arg);
+                name[cp - arg] = '\0';
+                z_HTTP_header_add(odr, &http_headers, name, value);
+            }
+            break;
+        case 'p':
+            xfree(post_buf);
+            post_buf = get_file(arg, &post_len);
+            method = "POST";
+            break;
+        case 'u':
+            if (strchr(arg, '/'))
+            {
+                char *cp = strchr(arg, '/');
+                char *user = odr_malloc(odr, 1 + cp - arg);
+                char *password = cp + 1;
+                memcpy(user, arg, cp - arg);
+                user[cp - arg] = '\0';
+                z_HTTP_header_add_basic_auth(odr, &http_headers, user,
+                                             password);
+            }
+            else
+                z_HTTP_header_add_basic_auth(odr, &http_headers, arg, 0);
+            break;
+        case 'x':
+            yaz_url_set_proxy(p, arg);
+            break;
+        case 0:
+            http_response = yaz_url_exec(p, arg, method, http_headers,
+                                         post_buf, post_len);
+            if (!http_response)
+                exit_code = 1;
+            else
+            {
+                fwrite(http_response->content_buf, 1,
+                       http_response->content_len, stdout);
+            }
+            break;
+        default:
+            usage();
+        }
+    }
+    yaz_url_destroy(p);
+    odr_destroy(odr);
+    exit(exit_code);
+}
+
+/*
+ * Local variables:
+ * c-basic-offset: 4
+ * c-file-style: "Stroustrup"
+ * indent-tabs-mode: nil
+ * End:
+ * vim: shiftwidth=4 tabstop=8 expandtab
+ */
+
