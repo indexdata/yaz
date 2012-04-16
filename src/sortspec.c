03dccce903176e61f889b1558834aa5a09f9f8c0
Adam Dickmeiss <adam@indexdata.dk>
2012-04-16 10:32:28 +0200

    Fix reading beyond string in yaz_sort_spec
    Spotted by Debians buildds output:
    https://buildd.debian.org/fetch.cgi?pkg=yaz&arch=mips&
     ver=4.2.30-1&stamp=1334563202&file=log

--- src/sortspec.c
+++ src/sortspec.c
@@ -123,7 +123,7 @@ Z_SortKeySpecList *yaz_sort_spec(ODR out, const char *arg)
                 sks->u.missingValueData->size = sks->u.missingValueData->len;
                 sks->u.missingValueData->buf = (unsigned char*)
                                           odr_strdup(out, sort_flags+i);
-                i += strlen(sort_flags+i);
+                i += strlen(sort_flags+i) - 1;
             }
         }
     }
