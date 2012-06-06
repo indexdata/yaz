301ebe26faf3818afe489ca21aefeabf1b238486
Dennis Schafroth <dennis@indexdata.com>
2012-06-06 12:07:34 +0200

    Implement missing function. already declared in API

--- src/zoom-c.c
+++ src/zoom-c.c
@@ -1021,6 +1021,15 @@ ZOOM_API(ZOOM_facet_field)
     return 0;
 }
 
+ZOOM_API(ZOOM_facet_field)
+    ZOOM_resultset_get_facet_by_index(ZOOM_resultset r, int index) {
+    int num = r->num_facets;
+    ZOOM_facet_field *facets = r->facets;
+    if (index >= 0 && index < num) {
+        return facets[index];
+    }
+    return 0;
+}
 
 ZOOM_API(ZOOM_facet_field *)
     ZOOM_resultset_facets(ZOOM_resultset r)
