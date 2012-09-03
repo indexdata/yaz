e418c3a09071c8ea587ce5e32b4f1dca18997dae
Adam Dickmeiss <adam@indexdata.dk>
2012-09-03 15:11:30 +0200

    GFS: increase default max message to 64 MB

--- src/statserv.c
+++ src/statserv.c
@@ -99,7 +99,7 @@ statserv_options_block control_block = {
     "tcp:@:9999",               /* default listener port */
     PROTO_Z3950,                /* default application protocol */
     900,                        /* idle timeout (seconds) */
-    1024*1024,                  /* maximum PDU size (approx.) to allow */
+    64*1024*1024,               /* maximum PDU size (approx.) to allow */
     "default-config",           /* configuration name to pass to backend */
     "",                         /* set user id */
     0,                          /* bend_start handler */
