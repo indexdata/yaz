indexdata/yaz

commit 7e90f2cc9a54a66e4bdf35056cf69241a93c9e16

yaz-client uses cs_create_host_proxy

client/client.c
@@ -702,10 +702,7 @@ static int session_connect_base(const char *arg, const char **basep)
     strncpy(type_and_host, arg, sizeof(type_and_host)-1);
     type_and_host[sizeof(type_and_host)-1] = '\0';
 
-    if (yazProxy)
-        conn = cs_create_host(yazProxy, 1, &add);
-    else
-        conn = cs_create_host(arg, 1, &add);
+    conn = cs_create_host_proxy(yazProxy, 1, &add, yazProxy);
     if (!conn)
     {
         printf("Could not resolve address %s\n", arg);

commit df7a36a7eadf9d8b797a1199fe5229d94e58fe1f

yaz-client: fix crash on connect
Problem introduced by 7e90f2cc9a54 .

client/client.c
@@ -702,7 +702,7 @@ static int session_connect_base(const char *arg, const char **basep)
     strncpy(type_and_host, arg, sizeof(type_and_host)-1);
     type_and_host[sizeof(type_and_host)-1] = '\0';
 
-    conn = cs_create_host_proxy(yazProxy, 1, &add, yazProxy);
+    conn = cs_create_host_proxy(arg, 1, &add, yazProxy);
     if (!conn)
     {
         printf("Could not resolve address %s\n", arg);