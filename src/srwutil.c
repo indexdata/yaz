indexdata/yaz

commit 5350317640042c3507172d28d14279595eb7a49f

Make yaz_use_attribute_create a public function
Useful when generating a facet response in a server.

src/srwutil.c
@@ -39,29 +39,37 @@ char *yaz_encode_sru_dbpath_odr(ODR out, const char *db)
     return dst;
 }
 
+Z_AttributeElement *yaz_string_element_create(ODR o, int type,
+                                              const char *value)
+{
+    Z_AttributeElement *element = (Z_AttributeElement*)
+        odr_malloc(o, sizeof(*element));
+    element->attributeType = odr_intdup(o, type);
+    element->attributeSet = odr_nullval();
+    element->which = Z_AttributeValue_complex;
+    element->value.complex = (Z_ComplexAttribute *)
+        odr_malloc(o, sizeof(Z_ComplexAttribute));
+    element->value.complex->num_list = 1;
+    element->value.complex->list = (Z_StringOrNumeric **)
+        odr_malloc(o, 1 * sizeof(Z_StringOrNumeric *));
+    element->value.complex->list[0] = (Z_StringOrNumeric *)
+        odr_malloc(o, sizeof(Z_StringOrNumeric));
+    element->value.complex->list[0]->which = Z_StringOrNumeric_string;
+    element->value.complex->list[0]->u.string = odr_strdup(o, value);
+    element->value.complex->semanticAction = 0;
+    element->value.complex->num_semanticAction = 0;
+    return element;
+}
+
 Z_AttributeList *yaz_use_attribute_create(ODR o, const char *name)
 {
-    Z_AttributeList *attributes= (Z_AttributeList *)
+    Z_AttributeList *attributes = (Z_AttributeList *)
         odr_malloc(o, sizeof(*attributes));
-    Z_AttributeElement ** elements;
-    attributes->num_attributes = 1;
-    elements = (Z_AttributeElement**)
+    Z_AttributeElement **elements = (Z_AttributeElement**)
         odr_malloc(o, attributes->num_attributes * sizeof(*elements));
-    elements[0] = (Z_AttributeElement*) odr_malloc(o,sizeof(**elements));
-    elements[0]->attributeType = odr_intdup(o, 1);
-    elements[0]->attributeSet = odr_nullval();
-    elements[0]->which = Z_AttributeValue_complex;
-    elements[0]->value.complex = (Z_ComplexAttribute *)
-        odr_malloc(o, sizeof(Z_ComplexAttribute));
-    elements[0]->value.complex->num_list = 1;
-    elements[0]->value.complex->list = (Z_StringOrNumeric **)
-        odr_malloc(o, 1 * sizeof(Z_StringOrNumeric *));
-    elements[0]->value.complex->list[0] = (Z_StringOrNumeric *)
-        odr_malloc(o, sizeof(Z_StringOrNumeric));
-    elements[0]->value.complex->list[0]->which = Z_StringOrNumeric_string;
-    elements[0]->value.complex->list[0]->u.string = odr_strdup(o, name);
-    elements[0]->value.complex->semanticAction = 0;
-    elements[0]->value.complex->num_semanticAction = 0;
+
+    elements[0] = yaz_string_element_create(o, 1, name);
+    attributes->num_attributes = 1;
     attributes->attributes = elements;
     return attributes;
 }

include/yaz/proto.h
@@ -193,6 +193,14 @@ void yaz_set_esn(Z_RecordComposition **comp_p, const char *esn, NMEM nmem);
 YAZ_EXPORT
 Z_DefaultDiagFormat *yaz_decode_init_diag(int no, Z_InitResponse *initrs);
 
+/** \brief creates AttributeList with type=1(use) and string value
+    \param o ODR for encoding
+    \param name use-value
+    \returns attribute list with only one use-attribute
+*/
+YAZ_EXPORT
+Z_AttributeList *yaz_use_attribute_create(ODR o, const char *name);
+
 YAZ_END_CDECL
 
 #include <yaz/prt-ext.h>

src/sru-p.h
@@ -35,8 +35,6 @@ void yaz_add_name_value_str(ODR o, char **name, char **value,  int *i,
 void yaz_add_name_value_int(ODR o, char **name, char **value, int *i,
                             char *a_name, Odr_int *val);
 
-Z_AttributeList *yaz_use_attribute_create(ODR o, const char *name);
-
 char *yaz_negotiate_sru_version(char *input_ver);
 
 void yaz_sru_facet_request(ODR, Z_FacetList **facetList,

src/sru_facet.c
@@ -259,32 +259,8 @@ void yaz_sru_facet_response(ODR o, Z_FacetList **facetList, xmlNodePtr n)
                             p_terms = p2;
                     }
                     if (index_name)
-                    {
-                        Z_AttributeList *al =
-                            (Z_AttributeList*) odr_malloc(o, sizeof(*al));
-                        Z_ComplexAttribute *ca =
-                            (Z_ComplexAttribute *) odr_malloc(o, sizeof(*ca));
-                        Z_AttributeElement *ae =
-                            (Z_AttributeElement *) odr_malloc(o, sizeof(*ae));
-                        al->num_attributes = 1;
-                        al->attributes = (Z_AttributeElement **)
-                            odr_malloc(o, sizeof(*al->attributes));
-                        al->attributes[0] = ae;
-                        ae->attributeSet = 0;
-                        ae->attributeType = odr_intdup(o, 1);
-                        ae->which = Z_AttributeValue_complex;
-                        ae->value.complex = ca;
-                        ca->num_semanticAction = 0;
-                        ca->semanticAction = 0;
-                        ca->num_list = 1;
-                        ca->list = (Z_StringOrNumeric **)
-                            odr_malloc(o, sizeof(*ca->list));
-                        ca->list[0] = (Z_StringOrNumeric *)
-                            odr_malloc(o, sizeof(**ca->list));
-                        ca->list[0]->which = Z_StringOrNumeric_string;
-                        ca->list[0]->u.string = index_name;
-                        ff->attributes = al;
-                    }
+                        ff->attributes = yaz_use_attribute_create(o,
+                                                                  index_name);
                     if (p_terms)
                     {
                         xmlNode *p;

src/srw.c
@@ -19,6 +19,7 @@
 #include <libxml/tree.h>
 #include <assert.h>
 #include <yaz/facet.h>
+#include <yaz/proto.h>
 #include "sru-p.h"
 
 char *yaz_negotiate_sru_version(char *input_ver)

commit 7fc72f3ae149e416a297ef1f55c09271056e98f1

Fix yaz_use_attribute_create (uninit memory)
Bug introduced by recent commit 5350317640042c3507172d28d142795.

src/srwutil.c
@@ -45,7 +45,7 @@ Z_AttributeElement *yaz_string_element_create(ODR o, int type,
     Z_AttributeElement *element = (Z_AttributeElement*)
         odr_malloc(o, sizeof(*element));
     element->attributeType = odr_intdup(o, type);
-    element->attributeSet = odr_nullval();
+    element->attributeSet = 0;
     element->which = Z_AttributeValue_complex;
     element->value.complex = (Z_ComplexAttribute *)
         odr_malloc(o, sizeof(Z_ComplexAttribute));
@@ -65,12 +65,11 @@ Z_AttributeList *yaz_use_attribute_create(ODR o, const char *name)
 {
     Z_AttributeList *attributes = (Z_AttributeList *)
         odr_malloc(o, sizeof(*attributes));
-    Z_AttributeElement **elements = (Z_AttributeElement**)
-        odr_malloc(o, attributes->num_attributes * sizeof(*elements));
 
-    elements[0] = yaz_string_element_create(o, 1, name);
     attributes->num_attributes = 1;
-    attributes->attributes = elements;
+    attributes->attributes = (Z_AttributeElement**)
+        odr_malloc(o, sizeof(*attributes->attributes));
+    attributes->attributes[0] = yaz_string_element_create(o, 1, name);
     return attributes;
 }
 