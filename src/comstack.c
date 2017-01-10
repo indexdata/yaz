f4821b82357ca6f721872ac7bd6894c837b5b27b
Adam Dickmeiss <adam@indexdata.dk>
2017-01-10 13:23:15 +0100

    HTTP decoding: ignore message length in head only mode

--- src/comstack.c
+++ src/comstack.c
@@ -376,13 +376,11 @@ static int cs_complete_http(const char *buf, int len, int head_only)
             {
                 ++j;
                 if (buf[j] == '1') /* 1XX */
-                    ;
+                    head_only = 1;
                 else if (!memcmp(buf + j, "204", 3))
-                    ;
+                    head_only = 1;
                 else if (!memcmp(buf + j, "304", 3))
-                    ;
-                else
-                    content_len = -1;
+                    head_only = 1;
                 break;
             }
     }
@@ -402,13 +400,13 @@ static int cs_complete_http(const char *buf, int len, int head_only)
             if (skip_crlf(buf, len, &i))
             {
                 /* inside content */
-                if (chunked)
+                if (head_only)
+                    return i;
+                else if (chunked)
                     return cs_read_chunk(buf, i, len);
                 else
                 {   /* not chunked ; inside body */
-                    if (content_len == -1)
-                        return 0;   /* no content length */
-                    else if (len >= i + content_len)
+                    if (len >= i + content_len)
                     {
                         return i + content_len;
                     }
