4eb8ff2cab1ee56e97014d51fa584e45b5f4f372
Adam Dickmeiss <adam@indexdata.dk>
2020-01-29 17:00:43 +0100

    Handle combining characters for Danmarc2 encoding YAZ-923

--- src/iconv_encode_danmarc.c
+++ src/iconv_encode_danmarc.c
@@ -19,12 +19,19 @@
 #include <yaz/xmalloc.h>
 #include "iconv-p.h"
 
-static size_t write_danmarc(yaz_iconv_t cd, yaz_iconv_encoder_t en,
-			    unsigned long x,
-			    char **outbuf, size_t *outbytesleft)
+#define MAX_COMP 4
+
+struct encoder_data
+{
+    unsigned long comp[MAX_COMP];
+    size_t sz;
+    unsigned long base_char;
+};
+
+static size_t write1(yaz_iconv_t cd, unsigned long x,
+                   char **outbuf, size_t *outbytesleft)
 {
     unsigned char *outp = (unsigned char *) *outbuf;
-
     if (x == '@' || x == '*')
     {
         if (*outbytesleft < 2)
@@ -78,13 +85,74 @@ static size_t write_danmarc(yaz_iconv_t cd, yaz_iconv_encoder_t en,
     return 0;
 }
 
+static size_t flush_danmarc(yaz_iconv_t cd, yaz_iconv_encoder_t e,
+                            char **outbuf, size_t *outbytesleft)
+{
+    struct encoder_data *w = (struct encoder_data *) e->data;
+    /* combining characters in reverse */
+    while (w->sz > 0)
+    {
+        size_t r = write1(cd, w->comp[w->sz - 1], outbuf, outbytesleft);
+        if (r)
+            return r;
+        w->sz--;
+    }
+    /* then base char */
+    if (w->base_char)
+    {
+        size_t r = write1(cd, w->base_char, outbuf, outbytesleft);
+        if (r)
+            return r; /* if we fail base_char is still there.. */
+        w->base_char = 0;
+    }
+    return 0;
+}
+
+static size_t write_danmarc(yaz_iconv_t cd, yaz_iconv_encoder_t e,
+			    unsigned long x,
+			    char **outbuf, size_t *outbytesleft)
+{
+    struct encoder_data *w = (struct encoder_data *) e->data;
+
+    if (x >= 0x300 && x <= 0x36F)
+    {
+        w->comp[w->sz++] = x;
+        if (w->sz < MAX_COMP)
+            return 0;
+    }
+    /* x is NOT a combining character. Flush previous sequence */
+    size_t r = flush_danmarc(cd, e, outbuf, outbytesleft);
+    if (r)
+        return r;
+    w->base_char = x;
+    return 0;
+}
+
+static void init_danmarc(yaz_iconv_encoder_t e)
+{
+    struct encoder_data *w = (struct encoder_data *) e->data;
+    w->base_char = 0;
+    w->sz = 0;
+}
+
+static void destroy_danmarc(yaz_iconv_encoder_t e)
+{
+    xfree(e->data);
+}
+
 yaz_iconv_encoder_t yaz_danmarc_encoder(const char *tocode,
                                         yaz_iconv_encoder_t e)
 
 {
     if (!yaz_matchstr(tocode, "danmarc"))
     {
+        struct encoder_data *data = (struct encoder_data *)
+            xmalloc(sizeof(*data));
+        e->data = data;
         e->write_handle = write_danmarc;
+        e->flush_handle = flush_danmarc;
+        e->init_handle = init_danmarc;
+        e->destroy_handle = destroy_danmarc;
         return e;
     }
     return 0;
