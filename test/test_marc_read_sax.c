b68d17d8545ce90b9bac18ee854f6dcb4ba0548d
Adam Dickmeiss <adam@indexdata.dk>
2022-11-18 17:12:06 +0100

    Further

--- test/test_marc_read_sax.c
+++ test/test_marc_read_sax.c
@@ -12,10 +12,14 @@
 #include <config.h>
 #endif
 
+struct ctx {
+    WRBUF wrbuf;
+};
 
 static void handler(yaz_marc_t mt, void *cb)
 {
-
+    struct ctx *ctx = cb;
+    yaz_marc_write_marcxml(mt, ctx->wrbuf);
 }
 
 static void tst1(void)
@@ -23,17 +27,18 @@ static void tst1(void)
     char *marcxml = "<collection xmlns=\"http://www.loc.gov/MARC21/slim\">\n"
      "<record>\n"
      "  <leader>00062cgm a2200037Ia 4500</leader>\n"
-     "  <datafield tag=\"092\" ind1=\" \" ind2=\" \">\n"
+     "  <datafield tag=\"092\" ind1=\" \" ind2=\"á\">\n"
      "    <subfield code=\"a\">DVD CHI 791.43</subfield>\n"
      "    <subfield code=\"b\">QI</subfield>\n"
      "  </datafield>\n"
      "</record>\n"
      "</collection>\n"
-     ;
-     
+    ;
+
+    struct ctx ctx;
+    ctx.wrbuf = wrbuf_alloc();
     yaz_marc_t mt = yaz_marc_create();
-    void *cb = mt;
-    yaz_marc_sax_t yt = yaz_marc_sax_new(mt, cb, handler);
+    yaz_marc_sax_t yt = yaz_marc_sax_new(mt, handler, &ctx);
     xmlSAXUserParseMemory(yaz_marc_sax_get(yt), yt, marcxml, strlen(marcxml));
     yaz_marc_sax_destroy(yt);
     yaz_marc_destroy(mt);
