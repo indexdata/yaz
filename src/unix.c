4236e9c3bc1821a39645ea3825904233e9e6c9a3
Adam Dickmeiss <adam@indexdata.dk>
2026-06-03 15:59:40 +0200

    lower max_recv_bytes from 128 MB to 16 MB

--- src/unix.c
+++ src/unix.c
@@ -155,6 +155,8 @@ COMSTACK unix_type(int s, int flags, int protocol, void *vp)
     p->f_straddr = unix_straddr;
     p->f_set_blocking = unix_set_blocking;
 
+    p->max_recv_bytes = 16 * 1024 * 1024;
+
     p->state = new_socket ? CS_ST_UNBND : CS_ST_IDLE; /* state of line */
     p->event = CS_NONE;
     p->cerrno = 0;
